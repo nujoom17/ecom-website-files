511d15a33fff7750807e6e6fbad92e6e020b42dc
nujoom17 <83825670+nujoom17@users.noreply.github.com>
2021-09-22 12:40:43 +0530

    Create slider-data.js

---
+++ src/SlideShow components/slider-data.js
@@ -0,0 +1,45 @@
+import first from "../media/img-5.jpg"
+import second from "../media/img-4.jpg"
+import third from "../media/img-3.png"
+import fourth from "../media/img-1.png"
+
+const people = [
+  {
+    id: 1,
+    image:
+      first,
+    name: '',
+    title: 'Worldwide Connectivity. At your fingertips',
+    quote:
+      'Your favourite products, explore the most diverse categories at the best prices! Delivered with care by our logistic partners across the globe',
+  },
+  {
+    id: 2,
+    image:
+      second,
+    name: '',
+    title: 'Service is our currency',
+    quote:
+      'Our customer service team is here to assist you 24x7, client satisfaction is our #1 priority',
+  },
+  {
+    id: 3,
+    image:
+      third,
+    name: '',
+    title: 'The leading e-commerce platform, accessibility and diversity drives us forward!',
+    quote:
+      'Serving the industry for over a decade and still growing rapidly, our clients and associates have contributed immensely to this venture.',
+  },
+  {
+    id: 4,
+    image:
+     fourth,
+    name: '',
+    title: 'We are full of surprises!',
+    quote:
+      'With each purchase, there\'s always something special awaiting you! Who doesn\'t like getting a surprise?',
+  },
+];
+
+export default people;
